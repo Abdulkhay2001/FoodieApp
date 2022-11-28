69133c3fd964c41dd157a64df2b00f75936fe1c9
Abdulkhay2001 <abdulkhay_asrorov@mail.ru>
2022-11-28 16:02:26 +0500

    add gridLayoutManager

--- app/src/main/java/com/example/foodie/ui/menu/MenuFragment.kt
+++ app/src/main/java/com/example/foodie/ui/menu/MenuFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.foodie.databinding.FragmentMenuBinding
 
 class MenuFragment : Fragment() {
@@ -26,6 +27,11 @@ class MenuFragment : Fragment() {
 
         _binding = FragmentMenuBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.rvMenu.layoutManager = GridLayoutManager(requireContext(), 2)
     }
 
     override fun onDestroyView() {
